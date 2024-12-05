12553dde794ce7e671fdc00b26ea5bd13f3d7d89
cosineblast <55855728+cosineblast@users.noreply.github.com>
2024-12-05 14:15:35 -0300

    Update locate_audio heuristic

--- src/locate_audio.py
+++ src/locate_audio.py
@@ -8,6 +8,8 @@ import json
 
 import fire
 
+SUPPORTED_SAMPLE_RATE = 48_000
+
 def locate_audio(haystack, needle):
     """
     Tries to find the best position for the audio `needle` in `haystack`.
@@ -16,13 +18,25 @@ def locate_audio(haystack, needle):
 
     correlation = scipy.signal.correlate(haystack, needle)
 
-    audio_start = int(np.argmax(correlation)) - len(needle) + 1
+    lowest_point = np.argmin(correlation)
+
+    # our heuristic is to find the lowest point of correlation then 
+    # find the highest point near that one.
+    highest_point = lowest_point + np.argmax(correlation[lowest_point:int(lowest_point+SUPPORTED_SAMPLE_RATE * 0.1)])
+
+    audio_start = highest_point - len(needle) + 1
 
     z_score = (np.max(correlation)- np.mean(correlation)) / np.std(correlation)
 
+    # sometimes things go really wrong and we end up estimating 
+    # that the audio starts somewhere impossible
+    # in that case, the right thing to do is to claim that we have absolutely
+    # no confidence on the result
+    if audio_start < 0:
+        return (0, 0)
+
     return (audio_start, float(z_score))
 
-SUPPORTED_SAMPLE_RATE = 48_000
 
 def _get_audio(path):
     sample_rate, data = scipy.io.wavfile.read(path)
@@ -44,7 +58,7 @@ def main(haystack_path: str, needle_path: str):
 
     haystack_path and needle_path must be strings pointing to the path of .wav files with a sample rate of 48k.
 
-    This scripts emits in stdout a json dictionary with three keys: 
+    This scripts emits in stdout a json dictionary with three keys:
     `offset`: a floating point value indicating the location of the `needle` file in the `haystack` file, in seconds
     `needle_duration`: a floating point value indicating the duration of the needle, in seconds
     `score`: a floating point number that represents the confidence of this script with the given input.
@@ -57,7 +71,7 @@ def main(haystack_path: str, needle_path: str):
     start, score = locate_audio(haystack, needle)
 
     print(json.dumps(
-        {'offset': start / SUPPORTED_SAMPLE_RATE, 
+        {'offset': start / SUPPORTED_SAMPLE_RATE,
          'score': score,
          'needle_duration': len(needle) / SUPPORTED_SAMPLE_RATE }))
 
